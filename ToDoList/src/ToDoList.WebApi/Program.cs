3ea6e798c05969e6018e91792b9cc4cd95b5a599
Jana Smutna <joe.smutna@gmail.com>
2025-10-09 20:41:48 +0200

    Lekce 03 - Úvod k metodám CRUD, vytvoření Data Transfer Object.

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -1,11 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args); //builder == postup, jak se aplikace vytvoří
+{
+    //Configure Dependency Injection (DI)
+    builder.Services.AddControllers();
+}
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "This is a test!");
-app.MapGet("/czechitas", () => "Vitej na kurzu Czechitas!");
-app.MapGet("/pozdrav/{jmeno}", (string jmeno) => $"Ahoj {jmeno}!");
-app.MapGet("/secti/{a:int}/{b:int}", (int a, int b) => $"Vysledek {a} + {b} = {(a + b)}");
-app.MapGet("/nazdarSvete", () => "Nazdar, svete :-)");
+var app = builder.Build();
+{
+    //Configure Middleware (HTTP request pipeline - co vše se dívá na requesty, které přijdou od klienta)
+    app.MapControllers();
+}
 
 app.Run();
