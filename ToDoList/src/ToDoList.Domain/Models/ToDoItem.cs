b7f0460c35c17d7064730b9b5a3289b307067e4d
Jana Smutna <joe.smutna@gmail.com>
2025-10-23 21:46:56 +0200

    Lekce 05 - Vytvoření databáze

--- ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
+++ ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
@@ -1,9 +1,14 @@
 namespace ToDoList.Domain.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class ToDoItem
 {
-    public int ToDoItemId { get; set; }
+    [Key] // specifikování klíče
+    public int ToDoItemId { get; set; } // Ef core hledá nejpravděpodobnější <id> nebo <nameId>
+    [Length(1, 50)]
     public string Name { get; set; } = null!;
+    [StringLength(250)]
     public string Description { get; set; } = null!;
     public bool IsCompleted { get; set; }
 }
