ec0bdfadd9bf1a360cd9911f3368ca7bb994967d
Jana Smutna <joe.smutna@gmail.com>
2025-11-26 22:19:21 +0100

    Assignment 09.1 - Lekce 09 a domácí úkol převedení synchronních metod na asynchronní

--- ToDoList/tests/ToDoList.Test/UnitTests/CreateTests.cs
+++ ToDoList/tests/ToDoList.Test/UnitTests/CreateTests.cs
@@ -1,5 +1,6 @@
 namespace ToDoList.Test.UnitTests;
 
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
@@ -11,16 +12,16 @@ using ToDoList.WebApi;
 public class CreateTests
 {
     [Fact]
-    public void Post_CreateValidRequest_ReturnsCreatedAtAction()
+    public async Task Post_CreateValidRequest_ReturnsCreatedAtAction()
     {
         // Arrange
-        var repository = Substitute.For<IRepository<ToDoItem>>();
+        var repository = Substitute.For<IRepositoryAsync<ToDoItem>>();
         var controller = new ToDoItemsController(repository);
 
         var dto = new ToDoItemCreateRequestDto("Post method", "Testing the Post/Create method - CreatedAtAction", false);
 
         // Act
-        var result = controller.Create(dto);
+        var result = await controller.Create(dto);
 
         // Assert
         var createdResult = Assert.IsType<CreatedAtActionResult>(result);
@@ -30,14 +31,14 @@ public class CreateTests
         Assert.Equal(dto.Name, responseDto.Name);
         Assert.Equal(dto.Description, responseDto.Description);
         Assert.Equal(dto.IsCompleted, responseDto.IsCompleted);
-        repository.Received(1).Create(Arg.Any<ToDoItem>());
+        await repository.Received(1).Create(Arg.Any<ToDoItem>());
     }
 
     [Fact]
-    public void Post_CreateUnhandledException_ReturnsInternalServerError()
+    public async Task Post_CreateUnhandledException_ReturnsInternalServerError()
     {
         // Arrange
-        var repository = Substitute.For<IRepository<ToDoItem>>();
+        var repository = Substitute.For<IRepositoryAsync<ToDoItem>>();
         var controller = new ToDoItemsController(repository);
 
         var dto = new ToDoItemCreateRequestDto("Post method", "Testing the Post/Create method - InternalServerError", false);
@@ -46,11 +47,11 @@ public class CreateTests
                   .Do(x => throw new Exception("Unexpected error"));
 
         // Act
-        var result = controller.Create(dto);
+        var result = await controller.Create(dto);
 
         // Assert
         var objectResult = Assert.IsType<ObjectResult>(result);
         Assert.Equal(StatusCodes.Status500InternalServerError, objectResult.StatusCode);
-        repository.Received(1).Create(Arg.Any<ToDoItem>());
+        await repository.Received(1).Create(Arg.Any<ToDoItem>());
     }
 }
