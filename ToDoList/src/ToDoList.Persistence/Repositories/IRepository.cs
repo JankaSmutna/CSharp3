91d7cd5c16221374dcd9fead58ac64509d418935
Jana Smutna <joe.smutna@gmail.com>
2025-11-26 22:19:21 +0100

    Assignment 09.1 - Lekce 09 a domácí úkol převedení synchronních metod na asynchronní

--- ToDoList/src/ToDoList.Persistence/Repositories/IRepository.cs
+++ ToDoList/src/ToDoList.Persistence/Repositories/IRepository.cs
@@ -14,3 +14,18 @@ where T : class
     public bool DeleteById(int id);
 
 }
+
+public interface IRepositoryAsync<T>
+where T : class
+{
+    public Task Create(T item);
+
+    public Task<IEnumerable<T>> Read();
+
+    public Task<T?> ReadById(int id);
+
+    public Task<bool> UpdateById(int id, T updatedItem);
+
+    public Task<bool> DeleteById(int id);
+
+}
