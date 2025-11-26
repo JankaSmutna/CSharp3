74c1f3aeb01a501360ea8ee6de5d7b427dc3f959
Jana Smutna <joe.smutna@gmail.com>
2025-11-26 22:52:41 +0100

    Reapply "Assignment 09.1 - Lekce 09 a domácí úkol převedení synchronních metod na asynchronní"
    This reverts commit 19061c41fe11fd6bcb56cc2c0521a0275ccf14e5.

--- ToDoList/tests/ToDoList.Test/UnitTests/ReadByIdTests.cs
+++ ToDoList/tests/ToDoList.Test/UnitTests/ReadByIdTests.cs
@@ -1,5 +1,6 @@
 namespace ToDoList.Test.UnitTests;
 
+using System.Threading.Tasks;
 using Xunit;
 using NSubstitute;
 using Microsoft.AspNetCore.Mvc;
@@ -18,10 +19,10 @@ public class ReadByIdTests
     [InlineData(4)]
     [InlineData(5)]
 
-    public void Get_ReadByIdWhenSomeItemAvailable_ReturnsOk(int id)
+    public async Task Get_ReadByIdWhenSomeItemAvailable_ReturnsOk(int id)
     {
         // Arrange
-        var repository = Substitute.For<IRepository<ToDoItem>>();
+        var repository = Substitute.For<IRepositoryAsync<ToDoItem>>();
         var controller = new ToDoItemsController(repository);
 
         var toDoItem = new ToDoItem
@@ -32,10 +33,10 @@ public class ReadByIdTests
             IsCompleted = false
         };
 
-        repository.ReadById(id).Returns(toDoItem);
+        repository.ReadById(id).Returns(Task.FromResult<ToDoItem?>(toDoItem));
 
         // Act
-        var result = controller.ReadById(id);
+        var result = await controller.ReadById(id);
 
         // Assert
         var okResult = Assert.IsType<OkObjectResult>(result.Result);
@@ -46,46 +47,46 @@ public class ReadByIdTests
         Assert.Equal($"Testing the Get/ReadById method - OK {id}", item.Description);
         Assert.False(item.IsCompleted);
 
-        repository.Received(1).ReadById(id);
+        await repository.Received(1).ReadById(id);
     }
 
     [Theory]
     [InlineData(999)]
     [InlineData(-1)]
-    public void Get_ReadByIdWhenItemIsNull_ReturnsNotFound(int id)
+    public async Task Get_ReadByIdWhenItemIsNull_ReturnsNotFound(int id)
     {
         // Arrange
-        var repository = Substitute.For<IRepository<ToDoItem>>();
+        var repository = Substitute.For<IRepositoryAsync<ToDoItem>>();
         var controller = new ToDoItemsController(repository);
 
-        repository.ReadById(id).Returns((ToDoItem?)null);
+        repository.ReadById(id).Returns(Task.FromResult<ToDoItem?>(null));
 
         // Act
         var result = controller.ReadById(id);
 
         // Assert
         Assert.IsType<NotFoundResult>(result.Result);
-        repository.Received(1).ReadById(id);
+        await repository.Received(1).ReadById(id);
     }
 
     [Theory]
     [InlineData(1)]
     [InlineData(42)]
-    public void Get_ReadByIdUnhandledException_ReturnsInternalServerError(int id)
+    public async Task Get_ReadByIdUnhandledException_ReturnsInternalServerError(int id)
     {
         // Arrange
-        var repository = Substitute.For<IRepository<ToDoItem>>();
+        var repository = Substitute.For<IRepositoryAsync<ToDoItem>>();
         var controller = new ToDoItemsController(repository);
 
         repository.When(r => r.ReadById(id))
                   .Do(x => throw new Exception("Unexpected error"));
 
         // Act
-        var result = controller.ReadById(id);
+        var result = await controller.ReadById(id);
 
         // Assert
         var objectResult = Assert.IsType<ObjectResult>(result.Result);
         Assert.Equal(StatusCodes.Status500InternalServerError, objectResult.StatusCode);
-        repository.Received(1).ReadById(id);
+        await repository.Received(1).ReadById(id);
     }
 }
