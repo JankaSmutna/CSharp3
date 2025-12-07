de1be0a3f9a8503fd2eae36f0caf0a1935804470
Jana Smutna <joe.smutna@gmail.com>
2025-12-07 19:20:44 +0100

    Final Project - Backend: Ošetření null výjimky vracením prázdného seznamu

--- ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
@@ -16,7 +16,7 @@ public class ToDoItemsClient : IToDoItemsClient
     public async Task<List<ToDoItemView>> ReadItemsAsync()
     {
         var toDoItemViews = new List<ToDoItemView>();
-        var response = await httpClient.GetFromJsonAsync<List<ToDoItemGetResponseDto>>("api/ToDoItems");
+        var response = await httpClient.GetFromJsonAsync<List<ToDoItemGetResponseDto>>("api/ToDoItems") ?? [];
 
         toDoItemViews = response.Select(dto => new ToDoItemView(dto.ToDoItemId, dto.Name, dto.Description, dto.IsCompleted, dto.Category)).ToList();
 
