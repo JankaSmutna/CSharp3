0799c85713844475ddc31b811880c0137c8fe18d
Jana Smutna <joe.smutna@gmail.com>
2025-12-07 20:30:44 +0100

    Assignment 10.1 - Lekce 10

--- ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
@@ -1,8 +1,7 @@
 namespace ToDoList.Frontend.Clients;
 
-using ToDoList.Frontend.Models;
 using ToDoList.Domain.DTOs;
-
+using ToDoList.Frontend.Models;
 
 public class ToDoItemsClient : IToDoItemsClient
 {
@@ -16,10 +15,41 @@ public class ToDoItemsClient : IToDoItemsClient
     public async Task<List<ToDoItemView>> ReadItemsAsync()
     {
         var toDoItemViews = new List<ToDoItemView>();
-        var response = await httpClient.GetFromJsonAsync<List<ToDoItemGetResponseDto>>("api/ToDoItems") ?? [];
+        var response = await httpClient.GetFromJsonAsync<List<ToDoItemGetResponseDto>>("api/ToDoItems");
 
-        toDoItemViews = response.Select(dto => new ToDoItemView(dto.ToDoItemId, dto.Name, dto.Description, dto.IsCompleted)).ToList();
+        toDoItemViews = response.Select(dto => new ToDoItemView()
+        {
+            Id = dto.ToDoItemId,
+            Name = dto.Name,
+            Description = dto.Description,
+            IsCompleted = dto.IsCompleted
+        }).ToList();
 
         return toDoItemViews;
+    }
+
+    public async Task<ToDoItemView?> ReadItemByIdAsync(int itemId)
+    {
+        var response = await httpClient.GetFromJsonAsync<ToDoItemGetResponseDto>($"api/ToDoItems/{itemId}");
+
+        if (response == null)
+        {
+            return null;
+        }
+
+        return new ToDoItemView
+        {
+            Id = response.ToDoItemId,
+            Name = response.Name,
+            Description = response.Description,
+            IsCompleted = response.IsCompleted
+        };
+    }
+
+    public async Task UpdateItemAsync(ToDoItemView item)
+    {
+        // try {}
+        var itemRequest = new ToDoItemUpdateRequestDto(item.Name, item.Description, item.IsCompleted);
+        var response = await httpClient.PutAsJsonAsync($"api/ToDoItems/{item.Id}", itemRequest);
     }
 }
--- ToDoList/src/ToDoList.Frontend/Models/ToDoItemView.cs
+++ ToDoList/src/ToDoList.Frontend/Models/ToDoItemView.cs
@@ -1,3 +1,9 @@
 namespace ToDoList.Frontend.Models;
 
-public record ToDoItemView(int Id, string Name, string Description, bool IsCompleted);
+public class ToDoItemView
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public bool IsCompleted { get; set; }
+}
